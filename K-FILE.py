71070855ff889ff1f0952f2f89223ae4c9751667
KHALID SHAIFULLAH <94977283+KHALID-404@users.noreply.github.com>
2023-02-16 20:50:35 +0600

    Create K-FILE.py

---
+++ K-FILE.py
@@ -0,0 +1,28 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+        os.system('xdg-open https://youtube.com/@Khalid_Vau')
+ 
+        from X-RANDOM import approval
+ 
+        approval()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        os.system('xdg-open https://youtube.com/@Khalid_Vau')
+ 
+        os.system('python file_enc.py')
